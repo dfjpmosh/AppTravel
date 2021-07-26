aa11c57062a402da337ddc0249cefe9ae8a46b87
Daniel Jimenez <dfjp_mosh@hotmail.com>
2021-07-25 21:28:09 -0500

    Estructura del proyecto

---
+++ AppTravel/AppTravel.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AppTravel.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ AppTravel/AppTravel.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppTravel.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
