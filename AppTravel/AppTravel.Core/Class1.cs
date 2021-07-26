f0eae05dced6d40cada88e243de4cbd653610ed7
Daniel Jimenez <dfjp_mosh@hotmail.com>
2021-07-25 21:31:15 -0500

    Limpieza de estructura

--- AppTravel/AppTravel.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AppTravel.Core
-{
-    public class Class1
-    {
-    }
-}
--- AppTravel/AppTravel.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AppTravel.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
